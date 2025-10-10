bbf5aaed3712ce637f72f1f38ca03b26a4d27d6b
nitzryan <nitzryan@gmail.com>
2025-10-09 23:19:25 -0500

    Modified Experiment code

--- BaseballModels/Model/Experiment_Hitters.py
+++ BaseballModels/Model/Experiment_Hitters.py
@@ -1,68 +1,65 @@
-from Data_Prep import Data_Prep
+from Data_Prep import Data_Prep, Player_IO
 from sklearn.model_selection import train_test_split # type: ignore
 import torch
-from Player_Dataset import Hitter_Dataset
-from Player_Model import RNN_Model, Classification_Loss
+import Prep_Map
+import Output_Map
 from torch.optim import lr_scheduler
+from Player_Dataset import Player_Dataset, Create_Test_Train_Datasets
 import Model_Train
+from Player_Model import RNN_Model
 from tqdm import tqdm
 from Constants import device
 import seaborn as sns
 from pylab import savefig
 from matplotlib import pyplot as plt
+import sys
 
 if __name__ == "__main__":
-    data_prep = Data_Prep()
-    hitters, inputs, outputs, max_input_size, dates = data_prep.Generate_IO_Hitters("WHERE (lastProspectYear<? OR lastMLBSeason<?) AND isHitter=?", (2025,2025,1))
+    if (len(sys.argv) != 2):
+        print(f"1 Argument expected (ExperimentHitters.py <filename no extension>), {len(sys.argv)} provided")
+        exit(1)
     
-    batch_size = 1000
-    x_scale : list[float] = [0,0.05,0.1,0.15,0.2]
-    y_scale : list[float] = [0,0.05,0.1,0.15,0.2]
+    filename = sys.argv[1]
+    
+    batch_size = 200
+    xs : list[int] = [1,2,3,4,5,6,7,8]
+    ys : list[int] = [22,23,24,25,26,27,28]
     
     data = []
     
-    x_label = "Bio Deviations"
-    y_label = "Playing Time Deviations"
+    x_label = "Num Layers"
+    y_label = "Hidden Size"
     
-    for x in tqdm(x_scale, desc=x_label):
+    data_prep = Data_Prep(Prep_Map.base_prep_map, Output_Map.base_output_map)
+    hitter_io_list = data_prep.Generate_IO_Hitters("WHERE lastMLBSeason<? AND signingYear<? AND isHitter=?", (2025,2015,1), use_cutoff=True)
+    train_dataset, test_dataset = Create_Test_Train_Datasets(hitter_io_list, 0.25, 0)
+    
+    
+    hitting_mutators = data_prep.Generate_Hitting_Mutators(batch_size, Player_IO.GetMaxLength(hitter_io_list))
+    
+    for hidden_size in tqdm(ys, desc=y_label):
         this_data = []
-        for y in tqdm(y_scale, desc=y_label, leave=False):
-            data_prep.Update_Mutators(hitbio_dev=x, hitpt_dev=y)
-            hitting_mutators = data_prep.Generate_Hitting_Mutators(batch_size, max_input_size)
+        for num_layers in tqdm(xs, desc=x_label, leave=False):
+            hitting_mutators = data_prep.Generate_Hitting_Mutators(batch_size, Player_IO.GetMaxLength(hitter_io_list))
             
-            x_train, x_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.25, random_state=4980)
-
-            train_lengths = torch.tensor([len(seq) for seq in x_train])
-            test_lengths = torch.tensor([len(seq) for seq in x_test])
-
-            x_train_padded = torch.nn.utils.rnn.pad_sequence(x_train)
-            x_test_padded = torch.nn.utils.rnn.pad_sequence(x_test)
-            y_train_padded = torch.nn.utils.rnn.pad_sequence(y_train)
-            y_test_padded = torch.nn.utils.rnn.pad_sequence(y_test)
-            train_hitters_dataset = Hitter_Dataset(x_train_padded, train_lengths, y_train_padded)
-            test_hitters_dataset = Hitter_Dataset(x_test_padded, test_lengths, y_test_padded)
             
-            # Setup Model
-            num_layers = 3
-            hidden_size = 30
-            network = RNN_Model(x_train_padded[0].shape[1], num_layers, hidden_size, hitting_mutators)
+            network = RNN_Model(train_dataset.get_input_size(), num_layers, hidden_size, hitting_mutators, data_prep=data_prep, is_hitter=True)
             network = network.to(device)
-            
-            optimizer = torch.optim.Adam(network.parameters(), lr=0.003)
-            scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=20, cooldown=5)
-            loss_function = Classification_Loss
-            
+
+            optimizer = torch.optim.Adam(network.parameters(), lr=0.004)
+            scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.9, patience=50, cooldown=10)
+
             num_epochs = 1000
-            training_generator = torch.utils.data.DataLoader(train_hitters_dataset, batch_size=batch_size, shuffle=True)
-            testing_generator = torch.utils.data.DataLoader(test_hitters_dataset, batch_size=batch_size, shuffle=False)
+            training_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            testing_generator = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
             
-            best_loss = Model_Train.trainAndGraph(network, training_generator, testing_generator, loss_function, optimizer, scheduler, num_epochs, logging_interval=10000, early_stopping_cutoff=40, should_output=False)
+            best_loss = Model_Train.trainAndGraph(network, training_generator, testing_generator, len(train_dataset), len(test_dataset), optimizer, scheduler, num_epochs, logging_interval=25, early_stopping_cutoff=50, should_output=False, save_last=False)
             this_data.append(best_loss)
         data.append(this_data)
         
         
-    heatmap = sns.heatmap(data, xticklabels=x_scale, yticklabels=y_scale, annot=True, fmt=".3f")
+    heatmap = sns.heatmap(data, xticklabels=xs, yticklabels=ys, annot=True, fmt=".3f")
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     figure = heatmap.get_figure()
-    figure.savefig('experiment.png', dpi=400)
+    figure.savefig(f'{filename}.png', dpi=400)
